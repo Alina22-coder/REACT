ef9bb38bce1b5f9c82c4921c7fb705ca73c07832
Alina_bkl <alinaburlakova06@gmail.com>
2025-09-05 18:52:48 +0300

    lesson 1 task 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import './App.css'
+import './App.css';
+
+const coursesAndDurationArray:{title:string, monthDuration:number}[] = [
+  {title: 'JavaScript Complex', monthDuration: 5},
+  {title: 'Java Complex', monthDuration: 6},
+  {title: 'Python Complex', monthDuration: 6},
+  {title: 'QA Complex', monthDuration: 4},
+  {title: 'FullStack', monthDuration: 7},
+  {title: 'Frontend', monthDuration: 4}
+];
 
 function App() {
   return (
-    <></>
+    <>
+      <ul>
+        {coursesAndDurationArray.map((course:{title:string, monthDuration:number}, index:number) => <li key={index}>{course.title} {course.monthDuration}</li>)}
+      </ul>
+    </>
   )
 }
 
-export default App
+export default App;
