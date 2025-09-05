06f144656a3de87808dda226b6575ca5717c0307
Alina_bkl <alinaburlakova06@gmail.com>
2025-09-05 18:27:05 +0300

    lesson 1 task 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import './App.css'
+import './App.css';
+
+const coursesTitleArray:string[] = [
+  'JavaScript Complex',
+  'Java Complex',
+  'Python Complex',
+  'QA Complex',
+  'Fullstack',
+  'Frontend'
+];
 
 function App() {
   return (
-    <></>
+    <>
+      <ul>
+        {coursesTitleArray.map((value, index) => <li key={index}>{value}</li>)}
+      </ul>
+    </>
   )
 }
 
-export default App
+export default App;
