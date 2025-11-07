ef285b38dbfc24eef717de45fc5aa983d7e2edc4
alina_bkl <alinaburlakova06@gmail.com>
2025-11-07 13:47:39 +0200

    state

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router-dom";
 
 export const MainLayout = () => {
   return (
-    <>
+    <div className="main">
         <Outlet/>
-    </>
+    </div>
   );
 };
     
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,15 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import { MainLayout } from "../layouts/MainLayout";
-import {Commercial} from "../components/Commercial/Commercial.tsx";
-import {Hotels} from "../components/Hotels/Hotels.tsx";
+import {ModuleState} from "../components/ModuleState/ModuleState.tsx";
 
 export const routes = createBrowserRouter([
     {
         path: "/",
         element: <MainLayout />,
         children: [
-            { index: true, element: <Commercial /> },
-            { path:"/hotels", element: <Hotels /> },
+            { index: true, element: <ModuleState /> },
         ],
     },
 ]);
